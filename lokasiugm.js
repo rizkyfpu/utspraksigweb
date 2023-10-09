6c311e20169d9a2ea69f41a28de98e8f557eb1dc
unknown <rizkyuluputty@gmail.com>
2023-10-09 14:56:43 +0700

    first commit

---
+++ lokasiugm.js
@@ -0,0 +1,12 @@
+var lokasigeojson = {
+"type": "FeatureCollection",
+"name": "lokasiugm",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::32749" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama Lokasi": "GSP UGM", "Lintang": "-7.769822846", "Bujur": "110.3778716" }, "geometry": { "type": "Point", "coordinates": [ 110.3778716, -7.769822846 ] } },
+{ "type": "Feature", "properties": { "Nama Lokasi": "Perpusat UGM", "Lintang": "-7.769024477", "Bujur": "110.3781234" }, "geometry": { "type": "Point", "coordinates": [ 110.3781234, -7.769024477 ] } },
+{ "type": "Feature", "properties": { "Nama Lokasi": "Departemen Teknik Geodesi UGM", "Lintang": "-7.764016715", "Bujur": "110.3726931" }, "geometry": { "type": "Point", "coordinates": [ 110.3726931, -7.764016715 ] } },
+{ "type": "Feature", "properties": { "Nama Lokasi": "Wisdom Park UGM", "Lintang": "-7.770219287", "Bujur": "110.3820464" }, "geometry": { "type": "Point", "coordinates": [ 110.3820464, -7.770219287 ] } },
+{ "type": "Feature", "properties": { "Nama Lokasi": "Masjid Kampus UGM", "Lintang": "-7.773003618", "Bujur": "110.3808707" }, "geometry": { "type": "Point", "coordinates": [ 110.3808707, -7.773003618 ] } }
+]
+}
